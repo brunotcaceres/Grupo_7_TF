de29d1aa3f5161874fdf304da70b0dae2c13c56b
eddycx <eddycx22@gmail.com>
2024-12-01 00:58:26 -0500

    das
    dadsd

--- ProyectoPrograWeb/platinoperu-frontend/src/componentes/Breadcrumb/Breadcrumb.jsx
+++ ProyectoPrograWeb/platinoperu-frontend/src/componentes/Breadcrumb/Breadcrumb.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import './Breadcrumb.css';
 
-const Breadcrumb = ({ path }) => {
+const Breadcrumb = () => {
+    const location = useLocation();
+    const pathnames = location.pathname.split('/').filter(x => x);
+
     return (
         <div className="breadcrumb">
-            {path.split('>').map((item, index) => (
-                <span key={index}>
-                    {item.trim()}
-                    {index < path.split('>').length - 1 && ' > '}
-                </span>
-            ))}
+            <Link to="/">Inicio</Link>
+            {pathnames.map((name, index) => {
+                const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
+                const isLast = index === pathnames.length - 1;
+                return (
+                    <span key={index}>
+                        {' > '}
+                        {isLast ? (
+                            <span>{name}</span>
+                        ) : (
+                            <Link to={routeTo}>{name}</Link>
+                        )}
+                    </span>
+                );
+            })}
         </div>
     );
 };
