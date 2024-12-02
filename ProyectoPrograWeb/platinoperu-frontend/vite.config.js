c638dc3b79b17fdbb5ef889c9c7ce0daa32caf86
Bruno Trujillo <bruno.tcaceres@gmail.com>
2024-12-02 11:14:02 -0500

    cambio para subir frontend

--- ProyectoPrograWeb/platinoperu-frontend/vite.config.js
+++ ProyectoPrograWeb/platinoperu-frontend/vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-        outDir: 'dist', // Carpeta donde se genera el build
-    },
+  base: '/Grupo_7_TF/'
 })
