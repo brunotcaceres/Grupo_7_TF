e3cc01191f9012ef68bdaa3726b4c48a233572a9
Bruno Trujillo <bruno.tcaceres@gmail.com>
2024-12-02 03:34:36 -0500

    cambio de loginform para verificacion con backend

--- ProyectoPrograWeb/platinoperu-frontend/src/paginas/MiCuenta/LoginForm.jsx
+++ ProyectoPrograWeb/platinoperu-frontend/src/paginas/MiCuenta/LoginForm.jsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MiCuenta.css';
 
 const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // Login successful, handle the login state (e.g., redirect to the dashboard)
+        console.log('Login successful', data);
+      } else {
+        // Login failed, show error message
+        setError(data.error);
+      }
+    } catch (error) {
+      setError('Error en la conexión con el servidor.');
+    }
+  };
+
   return (
     <div className="mi-cuenta-container">
       <h2>Acceder</h2>
-      <form className="mi-cuenta-form">
+      <form className="mi-cuenta-form" onSubmit={handleSubmit}>
         <label htmlFor="username">Nombre de usuario o correo electrónico *</label>
-        <input type="text" id="username" required />
+        <input 
+          type="text" 
+          id="username" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required 
+        />
 
         <label htmlFor="password">Contraseña *</label>
-        <input type="password" id="password" required />
+        <input 
+          type="password" 
+          id="password" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required 
+        />
+
+        {error && <p style={{ color: 'red' }}>{error}</p>}
 
         <div className="form-footer">
           <label>
